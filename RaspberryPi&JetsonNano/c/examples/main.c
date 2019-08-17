abadff5423b022fc4e1a55ec49b7143c09b3ebf7
Berenguer Montserrat <berenguer.95@gmail.com>
2019-08-17 10:04:42 +0200

    Put back the original comments in Makefile and example main.c

--- RaspberryPi&JetsonNano/c/examples/main.c
+++ RaspberryPi&JetsonNano/c/examples/main.c
@@ -17,7 +17,7 @@ int main(void)
     signal(SIGINT, Handler);
     printf("Uncomment the test you want to run in examples/main.c\n");
 
-    EPD_1in54_test();
+    // EPD_1in54_test();
     // EPD_1in54_V2_test();
     // EPD_1in54b_test();
     // EPD_1in54c_test();
