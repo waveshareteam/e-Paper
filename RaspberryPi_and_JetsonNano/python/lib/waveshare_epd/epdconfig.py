42ad4f53991cf7a1656e1100e3f09928a004eb37
txoof <aaron.ciuffo@gmail.com>
2019-10-30 20:34:30 +0100

    undo misguided edits

--- RaspberryPi_and_JetsonNano/python/lib/waveshare_epd/epdconfig.py
+++ RaspberryPi_and_JetsonNano/python/lib/waveshare_epd/epdconfig.py
@@ -68,7 +68,6 @@ class RaspberryPi:
         self.GPIO.setup(self.DC_PIN, self.GPIO.OUT)
         self.GPIO.setup(self.CS_PIN, self.GPIO.OUT)
         self.GPIO.setup(self.BUSY_PIN, self.GPIO.IN)
-        self.SPI = spidev.SpiDev(0, 0)
         self.SPI.max_speed_hz = 4000000
         self.SPI.mode = 0b00
 
