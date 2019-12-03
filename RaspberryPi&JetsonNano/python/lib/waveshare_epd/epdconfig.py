db0627b65fffd7c4e185a787f670ee130fd86e02
Brian Y <briancyiu@gmail.com>
2019-12-02 19:46:05 -0500

    Finished Rock64 class

--- RaspberryPi&JetsonNano/python/lib/waveshare_epd/epdconfig.py
+++ RaspberryPi&JetsonNano/python/lib/waveshare_epd/epdconfig.py
@@ -42,12 +42,41 @@ class Rock64:
 	def __init__(self):
 		import spidev
 		import R64.GPIO as GPIO
-
+		
 		self.GPIO = GPIO
 		self.SPI = spidev.SpiDev(32766, 1)
 
 	def digital_write(self, pin, value):
 		self.GPIO.output(pin, value)
+
+	def digital_read(self, pin):
+        return self.GPIO.input(pin)
+
+    def delay_ms(self, delaytime):
+        time.sleep(delaytime / 1000.0)
+
+    def spi_writebyte(self, data):
+        self.SPI.writebytes(data)
+
+    def module_init(self):
+        self.GPIO.setmode(self.GPIO.BCM)
+        self.GPIO.setwarnings(False)
+        self.GPIO.setup(self.RST_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.DC_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.CS_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.BUSY_PIN, self.GPIO.IN)
+        self.SPI.max_speed_hz = 4000000
+        self.SPI.mode = 0b00
+        return 0
+
+    def module_exit(self):
+        logging.debug("spi end")
+        self.SPI.close()
+
+        logging.debug("close 5V, Module enters 0 power consumption ...")
+        self.GPIO.output(self.RST_PIN, 0)
+        self.GPIO.output(self.DC_PIN, 0)
+        self.GPIO.cleanup()
 
 
 class RaspberryPi:
