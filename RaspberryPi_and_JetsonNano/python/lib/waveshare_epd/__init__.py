ab8216bc3ee4eec2ad90ee3ba056aa3f05c1f8c4
txoof <aaron.ciuffo@gmail.com>
2019-12-22 11:44:45 +0100

    upate init file

---
+++ RaspberryPi_and_JetsonNano/python/lib/waveshare_epd/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/envthon
+#__all__ =  ['epd5in83']
+from . import epd1in02
+from . import epd1in54b
+from . import epd1in54c
+from . import epd1in54
+from . import epd1in54_V2
+from . import epd2in13bc
+from . import epd2in13d
+from . import epd2in13
+from . import epd2in13_V2
+from . import epd2in7b
+from . import epd2in7
+from . import epd2in9bc
+from . import epd2in9d
+from . import epd2in9
+from . import epd4in2bc
+from . import epd4in2
+from . import epd5in83bc
+from . import epd5in83
+from . import epd7in5bc
+from . import epd7in5bc_V2
+from . import epd7in5
+from . import epd7in5_V2
+from . import epdconfig
