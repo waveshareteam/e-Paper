82a87d53f51e9f233565fd0518524a163877f838
Bryan Bassett <bryanbassett@gmail.com>
2020-03-29 01:32:57 -0400

    delay SPI.open until module_init

--- RaspberryPi&JetsonNano/python/lib/waveshare_epd/epdconfig.py
+++ RaspberryPi&JetsonNano/python/lib/waveshare_epd/epdconfig.py
@@ -45,9 +45,7 @@ class RaspberryPi:
         import RPi.GPIO
 
         self.GPIO = RPi.GPIO
-
-        # SPI device, bus = 0, device = 0
-        self.SPI = spidev.SpiDev(0, 0)
+        self.SPI = spidev.SpiDev()
 
     def digital_write(self, pin, value):
         self.GPIO.output(pin, value)
@@ -68,6 +66,9 @@ class RaspberryPi:
         self.GPIO.setup(self.DC_PIN, self.GPIO.OUT)
         self.GPIO.setup(self.CS_PIN, self.GPIO.OUT)
         self.GPIO.setup(self.BUSY_PIN, self.GPIO.IN)
+
+        # SPI device, bus = 0, device = 0
+        self.SPI.open(0, 0)
         self.SPI.max_speed_hz = 4000000
         self.SPI.mode = 0b00
         return 0
