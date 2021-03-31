8bb4c14351e72026217707a6618495b5d508e320
missionfloyd <missionfloyd@users.noreply.github.com>
2021-03-31 07:58:48 -0600

    Detect platform

--- RaspberryPi_JetsonNano/python/setup.py
+++ RaspberryPi_JetsonNano/python/setup.py
@@ -1,16 +1,19 @@
 import sys
 from setuptools import setup
+
+dependencies = ['Pillow', 'numpy']
+
+if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
+    dependencies.extend(['RPi.GPIO', 'spidev'])
+else:
+    dependencies.extend(['Jetson.GPIO', 'smbus'])
+
 setup(
     name='waveshare-epd',
     description='Waveshare e-Paper Display',
     author='Waveshare',
     package_dir={'': 'lib'},
     packages=['waveshare_epd'],
-    install_requires=[
-        'RPi.GPIO',
-        'spidev',
-        'Pillow',
-        'numpy'
-    ],
+    install_requires=dependencies,
 )
 
