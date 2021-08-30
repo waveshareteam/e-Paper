2c1334836e01e2ee9c3ddd97f071691393c9c8cb
tkameroski <76920240+tkameroski@users.noreply.github.com>
2021-08-30 11:23:22 -0400

    Update epd7in5_V2.py

--- RaspberryPi_JetsonNano/python/lib/waveshare_epd/epd7in5_V2.py
+++ RaspberryPi_JetsonNano/python/lib/waveshare_epd/epd7in5_V2.py
@@ -129,15 +129,15 @@ class EPD:
 
     def ReadBusy(self):
         logger.debug("e-Paper busy")
-        # self.send_command(0x71)
+	# self.send_command(0x71)
 	iter = 0
         while epdconfig.digital_read(self.busy_pin) == 0:
-            # self.send_command(0x71)
-	    epdconfig.delay_ms(100)
-            iter += 1
-	    if iter > 150:
-                logger.info("Forced e-paper busy release")
-		break
+		# self.send_command(0x71)
+		epdconfig.delay_ms(100)
+		iter += 1
+		if iter > 150:
+			logger.info("Forced e-paper busy release")
+			break
         logger.debug("e-Paper busy release")
         
     def SetLut(self, lut_vcom, lut_ww, lut_bw, lut_wb, lut_bb):
