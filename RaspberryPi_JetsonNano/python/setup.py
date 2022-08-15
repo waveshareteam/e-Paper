60ff8c214e4a0611cee753e5c3b25261f29069bb
SSYYL <1032789891@qq.com>
2022-08-15 11:18:26 +0800

    fix

--- RaspberryPi_JetsonNano/python/setup.py
+++ RaspberryPi_JetsonNano/python/setup.py
@@ -5,6 +5,8 @@ dependencies = ['Pillow']
 
 if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
     dependencies += ['RPi.GPIO', 'spidev']
+elif os.path.exists('/sys/bus/platform/drivers/gpio-x3'):
+    dependencies += ['Hobot.GPIO', 'spidev']
 else:
     dependencies += ['Jetson.GPIO']
 
