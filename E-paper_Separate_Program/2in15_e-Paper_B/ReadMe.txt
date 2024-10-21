af0c7dc2b04220b04dae75838cff47f9046d49b5
shhds <1873145421@qq.com>
2024-10-21 16:27:01 +0800

    ADD 2in15 e-Paper (G)

--- E-paper_Separate_Program/2in15_e-Paper_B/ReadMe.txt
+++ E-paper_Separate_Program/2in15_e-Paper_B/ReadMe.txt
@@ -1,7 +1,7 @@
 中文：
 使用方法详见中文Wiki
-https://www.waveshare.net/wiki/3.52inch_e-Paper_HAT_(B)
+https://www.waveshare.net/wiki/2.15inch_e-Paper_HAT+_(B)
 
 English：
 See the English Wiki for details
-https://www.waveshare.com/wiki/3.52inch_e-Paper_HAT_(B)
+https://www.waveshare.com/wiki/2.15inch_e-Paper_HAT+_(B)
---
+++ E-paper_Separate_Program/2in15_e-Paper_G/STM32-F103ZET6/User/e-Paper/EPD_2in15g.h
@@ -0,0 +1,32 @@
+/*****************************************************************************
+* | File      	:   EPD_2in15g.h
+* | Author      :   Waveshare team
+* | Function    :   2inch15 e-paper (G)
+* | Info        :
+*----------------
+* |	This version:   V1.0
+* | Date        :   2024-08-07
+* | Info        :
+* -----------------------------------------------------------------------------
+******************************************************************************/
+#ifndef __EPD_2IN15G_H_
+#define __EPD_2IN15G_H_
+
+#include "DEV_Config.h"
+
+// Display resolution
+#define EPD_2IN15G_WIDTH       160
+#define EPD_2IN15G_HEIGHT      296
+
+// Color
+#define  EPD_2IN15G_BLACK   0x0
+#define  EPD_2IN15G_WHITE   0x1
+#define  EPD_2IN15G_YELLOW  0x2
+#define  EPD_2IN15G_RED     0x3
+
+void EPD_2IN15G_Init(void);
+void EPD_2IN15G_Clear(UBYTE color);
+void EPD_2IN15G_Display(UBYTE *Image);
+void EPD_2IN15G_Sleep(void);
+
+#endif
