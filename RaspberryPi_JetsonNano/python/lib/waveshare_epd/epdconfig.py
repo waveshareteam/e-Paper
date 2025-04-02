aefc79b73e884287f163eb7df19eb81beaedcfc0
Jacobe Zang <jacobe.zang@wesion.com>
2025-04-02 17:12:24 +0800

    src: add Khadas VIM1S support
    Signed-off-by: Jacobe Zang <jacobe.zang@wesion.com>

--- RaspberryPi_JetsonNano/python/lib/waveshare_epd/epdconfig.py
+++ RaspberryPi_JetsonNano/python/lib/waveshare_epd/epdconfig.py
@@ -1,5 +1,5 @@
 # /*****************************************************************************
-# * | File        :	  epdconfig.py
+# * | File        :       epdconfig.py
 # * | Author      :   Waveshare team
 # * | Function    :   Hardware underlying interface
 # * | Info        :
@@ -102,6 +102,10 @@ class KhadasVIM4(Khadas):
 class KhadasVIM3(Khadas):
     def __init__(self):
         super().__init__(rst=462, dc=461, cs=460, busy=464, pwr=496, spi_bus=2)
+
+class KhadasVIM1S(Khadas):
+    def __init__(self):
+        super().__init__(rst=456, dc=457, cs=470, busy=509, pwr=502, spi_bus=1)
 
 class RaspberryPi:
     # Pin definition
@@ -374,6 +378,8 @@ def get_khadas_model():
                 return KhadasVIM4()
             elif "VIM3" in model:
                 return KhadasVIM3()
+            elif "VIM1S" in model:
+                return KhadasVIM1S()
     return None
 
 if sys.version_info[0] == 2:
